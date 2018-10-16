29a2047582cc2aa2824d863bc0439418e7b71d8a
hamer073 <hamer073@morris.umn.edu>
2018-10-16 16:36:11 -0500

    Add comments so that it's clearer what's all happening

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -5,9 +5,14 @@ import java.net.*;
 
 public class EchoClient {
 
+  // Set the port number we expect to connect to as a final variable
+  // If we can't connect to that port we assume the server is down
   public static final int portNumber = 6013;
 
   public static void main(String[] args) {
+
+    // Set the server ip/domain we want to connect to.
+    // If the user doesn't specify an ip/domain we just assume their connecting on localhost
     String server;
 
     if(args.length == 0) {
@@ -16,11 +21,15 @@ public class EchoClient {
       server = args[0];
     }
 
+    // Try to connect to the server
     try {
+
+      // Create a new socket and InputStream/OutputStream based on that socket
       Socket socket = new Socket(server, portNumber);
       OutputStream userIn = socket.getOutputStream();
       InputStream response = socket.getInputStream();
 
+      // Read from stdin, send it to the server, and then write to stdout whatever the server sends back
       int systemIn;
       while((systemIn = System.in.read()) != -1) {
         userIn.write(systemIn);
@@ -28,9 +37,13 @@ public class EchoClient {
         System.out.write(serverResponse);
       }
 
+      // Some cleanup so that hopefully nothing horrible happens
       socket.shutdownOutput();
       System.out.flush();
-    } catch (ConnectException ce) {
+    }
+
+    // Some exception catching so we at least have some idea of what went wrong
+      catch (ConnectException ce) {
       System.out.println("We were unable to connect to " + server);
       System.out.println("You should make sure the server is running.");
     } catch (IOException ioe)  {
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,31 +5,43 @@ import java.io.*;
 
 public class EchoServer {
 
+  // Set the port the server is going to listen on
+  // We never want this to change so we set it as final
   public static final int portNumber = 6013;
 
   public static void main(String[] args){
 
     try{
 
+      // Create the server socket that listens for requests from a client
       ServerSocket serverSocket = new ServerSocket(portNumber);
 
+      // We loop infintely here since that's what servers do
       while(true) {
 
+        // The code will actually just stay here until a client tries to connect
+        // We also output a friendly message letting people know that a client has connected
         Socket client = serverSocket.accept();
         System.out.println("Recieved a request");
 
+        // Create the input and output streams that we'll read and write from
         InputStream clientRequest = client.getInputStream();
         OutputStream serverResponse = client.getOutputStream();
 
+        // Have a temporary int here to hold whatever byte we get from the client
         int tempByte;
         while((tempByte = clientRequest.read()) != -1) {
+          // Once we get the byte, we immediately send it back
           serverResponse.write(tempByte);
         }
 
+        // Shut down the connection with the client once it's finished talking to us
         client.shutdownOutput();
 
       }
-    } catch(IOException ioe) {
+    }
+    // Some more exception catching
+     catch(IOException ioe) {
       System.out.println("We caught an unexpected exception");
       System.err.println(ioe);
     }
